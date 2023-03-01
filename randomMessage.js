ec8a777d88b8f1c155e81ea7ac477f1fac999aa1
ginacostanzo <106881838+ginacostanzo@users.noreply.github.com>
2023-03-01 12:22:17 -0500

    Add arrays of activities, locations, and companions

---
+++ randomMessage.js
@@ -0,0 +1,79 @@
+const activities = [
+    'hiking', 
+    'eating the local cuisine', 
+    'chasing waterfalls', 
+    'relaxing on a beach', 
+    'swimming with sharks',
+    'dancing',
+    'falling in love',
+    'taking a bike tour',
+    'drinking wine',
+    'exploring ruins',
+    'bungee jumping',
+    'on a boat',
+    'learning to bake',
+    'seeing wild animals',
+    'getting lost',
+    'seeing a show',
+    'surfing',
+    'having a photoshoot',
+    'learning a language',
+    'at a spa',
+
+];
+
+const locations = [
+    'Italy',
+    'Greece',
+    'Costa Rica',
+    'Iceland',
+    'Switzerland',
+    'France',
+    'Thailand',
+    'Africa',
+    'Egypt',
+    'Peru',
+    'New York',
+    'Vietnam',
+    'China',
+    'Japan',
+    'Las Vegas',
+    'Antarctica',
+    'Hawaii',
+    'Morocco',
+    'Portugal',
+    'Aruba',
+    'New Zealand',
+    'Australia',
+    'Ireland',
+    'Scotland',
+    'Spain',
+    'Germany',
+    'South Korea',
+    'Croatia',
+    'England',
+    'Sweden',
+    'Norway',
+    'Belgium',
+    'The Netherlands',
+    'Nicaragua',
+    'The Bahamas',
+    'Mexico',
+    'Chile'
+];
+
+const companions = [
+    'yourself',
+    'a stranger',
+    'your best friend',
+    'your mom',
+    'your boss',
+    'your ex',
+    'your pet',
+    'your dad',
+    'your sibling'
+];
+
+
+console.log('Your next vacation is...');
+
